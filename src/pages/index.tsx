c7915311260aa6cf2dc5af5b0779627df3eaa142
Jeff Registre <jeffreyregistre@gmail.com>
2020-05-28 00:48:01 -0400

    Delete firebase and cloudbuild

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,37 +3,9 @@ import {graphql as gql, Link, useStaticQuery, withPrefix} from "gatsby"
 
 import "./styles/index.sass"
 
-// function addNetlifyIdentityScript(): void {
-//     if(document) {
-//         const script = document.createElement("script");
-//         script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js";
-//         // script.async = true
-//         document.body.appendChild(script);
-//     }
-// }
-//
-// function addAdminRedirectScript(): void {
-//     if(document) {
-//         const script = document.createElement("script");
-//         script.async = true;
-//         script.innerText = `
-//         if (window.netlifyIdentity) {
-//             window.netlifyIdentity.on("init", user => {
-//                 if (!user) {
-//                     window.netlifyIdentity.on("login", () => {
-//                         document.location.href = "/admin/";
-//                     });
-//                 }
-//             })
-//         }
-//     `
-//         document.body.appendChild(script);
-//     }
-// }
-
 const IndexPage = () => {
     const data = useStaticQuery(gql`
-        query FooobarQuery {
+        query HomeQuery {
             file(relativePath: { eq: "coming-soon.jpg" }) {
                 childImageSharp {
                     fluid {
@@ -49,7 +21,6 @@ const IndexPage = () => {
     return (
 
         <>
-            <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"/>
             <main>
                 <div className="wrapper"
                      style={{backgroundImage: `url(${data.file.childImageSharp.fluid.originalImg})`}}>
