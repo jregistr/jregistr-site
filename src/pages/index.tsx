3f2f73e1c59fbc3312635678c00f18b02c1353c5
Jeff Registre <jeffreyregistre@gmail.com>
2020-05-28 00:15:29 -0400

    html.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,37 +3,35 @@ import {graphql as gql, Link, useStaticQuery, withPrefix} from "gatsby"
 
 import "./styles/index.sass"
 
-function addNetlifyIdentityScript(): void {
-    if(document) {
-        const script = document.createElement("script");
-        script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js";
-        // script.async = true
-        document.body.appendChild(script);
-    }
-}
-
-function addAdminRedirectScript(): void {
-    if(document) {
-        const script = document.createElement("script");
-        script.async = true;
-        script.innerText = `
-        if (window.netlifyIdentity) {
-            window.netlifyIdentity.on("init", user => {
-                if (!user) {
-                    window.netlifyIdentity.on("login", () => {
-                        document.location.href = "/admin/";
-                    });
-                }
-            })
-        }
-    `
-        document.body.appendChild(script);
-    }
-}
+// function addNetlifyIdentityScript(): void {
+//     if(document) {
+//         const script = document.createElement("script");
+//         script.src = "https://identity.netlify.com/v1/netlify-identity-widget.js";
+//         // script.async = true
+//         document.body.appendChild(script);
+//     }
+// }
+//
+// function addAdminRedirectScript(): void {
+//     if(document) {
+//         const script = document.createElement("script");
+//         script.async = true;
+//         script.innerText = `
+//         if (window.netlifyIdentity) {
+//             window.netlifyIdentity.on("init", user => {
+//                 if (!user) {
+//                     window.netlifyIdentity.on("login", () => {
+//                         document.location.href = "/admin/";
+//                     });
+//                 }
+//             })
+//         }
+//     `
+//         document.body.appendChild(script);
+//     }
+// }
 
 const IndexPage = () => {
-    addNetlifyIdentityScript();
-    addAdminRedirectScript();
     const data = useStaticQuery(gql`
         query FooobarQuery {
             file(relativePath: { eq: "coming-soon.jpg" }) {
